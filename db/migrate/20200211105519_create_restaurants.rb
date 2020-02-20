4e102639d1d23e4ab295344b42598a3d79360db0
Jamie <burnseh@outlook.com>
2020-02-20 12:54:37 +0200

    Added styling updated bundler

--- db/migrate/20200211105519_create_restaurants.rb
+++ db/migrate/20200211105519_create_restaurants.rb
@@ -7,3 +7,12 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
     end
   end
 end
+
+
+# insert into restaurants (id, name, created_at, updated_at ) values (1, 'PT_FOODOO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+# insert into restaurants (id, name, created_at, updated_at ) values (2, 'PT_KYLYMÄ', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+# insert into restaurants (id, name, created_at, updated_at ) values (3, 'PT_NAPA', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+# insert into restaurants (id, name, created_at, updated_at ) values (4, 'PT_FOOBAR', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+# insert into restaurants (id, name, created_at, updated_at ) values (5, 'PT_KASTARI', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+# select * from restaurants;
