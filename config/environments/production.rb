d1e36ed4f65752a15df04d7cd5c0a30604c545d0
Jamie <burnseh@outlook.com>
2020-02-22 17:32:20 +0200

    Adding image email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,12 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { :host => 'posttaste.herokuapp.com' }  
+  config.action_mailer.default_url_options = { :host => 'posttaste.herokuapp.com' }
+  
+  config.action_controller.asset_host = 'posttaste.herokuapp.com'
+
+  config.action_mailer.asset_host = 'posttaste.herokuapp.com'
+
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = true  
